c102569bd51d8b604fe74a0ee6599bd66708ab8b
slynia shi <jacobnickerson817@gmail.com>
2024-01-04 20:28:24 -0500

    Scraping update

--- scripts.py
+++ scripts.py
@@ -5,27 +5,22 @@ import re
 
 def wiki_scrape(item_name):
     url = f"https://minecraft.wiki/w/{item_name}"
-    response = requests.get(url)
-    return response.text
-
-
-def wiki_parse(wiki_html):
-    data = []
+    wiki_html = requests.get(url).text
+    data = {}
     soup = BeautifulSoup(wiki_html, "html.parser")
     with open("wiki_page.txt", "w", encoding="utf-8") as file:
-       file.write(str(soup))
+        file.write(str(soup))
     if description := re.search(r"content=\"(.+)\" name=\"description\"", str(soup)):
-       data.append(description.group(1))
+        data["description"] = description.group(1)
     if block_image := re.search(r"content=\"(.+)\" property=\"og:image\"", str(soup)):
-       data.append(block_image.group(1))
-    else:
-        print("DIE")
+        data["image"] = block_image.group(1)
+    if crafting_recipe := re.search(r"stink butt weiner", str(soup)):
+        data["crafting_recipe"] = crafting_recipe.group(1)
     return data
 
 def main():
     block_name = input("Input: ")
-    print(wiki_parse(wiki_scrape(block_name)))
-
+    print(wiki_scrape(block_name))
 
 if __name__ == "__main__":
     main()
