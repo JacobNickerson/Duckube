dcb2dcdabf72c7560b186c0ae78277cf72d5322c
JacobNickerson <jacobnickerson817@gmail.com>
2024-01-08 16:45:55 -0500

    Search slash command added

--- scripts.py
+++ scripts.py
@@ -4,23 +4,44 @@ import re
 
 
 def wiki_scrape(item_name):
-    url = f"https://minecraft.wiki/w/{item_name}"
-    wiki_html = requests.get(url).text
-    data = {}
-    soup = BeautifulSoup(wiki_html, "html.parser")
-    with open("wiki_page.txt", "w", encoding="utf-8") as file:
-        file.write(str(soup))
+    found = False
+    item_data = {}
+    while found == False:
+        url = f"https://minecraft.wiki/?search={item_name.replace(" ", "+")}"
+        wiki_html = requests.get(url).text
+        soup = BeautifulSoup(wiki_html, "html.parser")
+        with open("wiki_page.txt", "w", encoding="utf-8") as file:
+            file.write(str(soup))
+
+        if redirection := re.search(r"Did you mean: <a href=\"/w/Special:Search\?fulltext=1\&amp;profile=default\&amp;search=([^\"]+)\"", str(soup)):
+            item_name = redirection.group(1)
+        elif redirection := re.search(r"Showing results for <a href=\"/w/Special:Search\?fulltext=1\&amp;profile=default\&amp;search=([^\"]+)\"", str(soup)):
+            item_name = redirection.group(1)
+        else:
+            found = True
+            item_data["url"] = url
+
+    if not_found := re.search(f"<title>Search results for \"{item_name}\" – Minecraft Wiki</title>", str(soup)):
+        return {"error": True}
+    
+    if title := re.search(r"<title>(.+) – Minecraft Wiki</title>", str(soup)):
+        item_data["title"] = title.group(1)
     if description := re.search(r"content=\"(.+)\" name=\"description\"", str(soup)):
-        data["description"] = description.group(1)
+        item_data["description"] = description.group(1)
     if block_image := re.search(r"content=\"(.+)\" property=\"og:image\"", str(soup)):
-        data["image"] = block_image.group(1)
+        item_data["image"] = block_image.group(1)
     if crafting_recipe := re.search(r"stink butt weiner", str(soup)):
-        data["crafting_recipe"] = crafting_recipe.group(1)
-    return data
+        item_data["crafting_recipe"] = crafting_recipe.group(1)
+
+        
+
+    return item_data
+
 
 def main():
     block_name = input("Input: ")
     print(wiki_scrape(block_name))
+
 
 if __name__ == "__main__":
     main()
--- test_file.py
+++ test_file.py
@@ -1,17 +1,7 @@
-import re
-import shutil
-import requests
+import dotenv
+import os
 
 
-wanted_text = '<meta content="Dirt is a block found abundantly in most biomes under a layer of grass blocks at the top of the Overworld." name="description"/>'
-if matches := re.search(r'meta content="(.+)" name="description"', wanted_text):
-    print(matches.group(1))
-else:
-    print("You didn't find it dipshit")
-
-r = requests.get("https://minecraft.wiki/images/Glass_JE4_BE2.png?fb219", stream=True)
-
-if r.status_code == 200:
-    with open("images/block.png", "wb") as f:
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
+dotenv.load_dotenv()
+server_token = os.getenv("server_token")
+print(server_token)
